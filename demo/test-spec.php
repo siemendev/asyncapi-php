45876f9f560687a8c3fe78a90923c1e9e5556e78
Patrick Siemen <post@patrick-siemen.de>
2025-05-27 12:57:38 +0200

    update test-spec.php to retrieve and resolve the first message from test_publish operation

--- demo/test-spec.php
+++ demo/test-spec.php
@@ -5,6 +5,6 @@ use Siemendev\AsyncapiPhp\Spec\Model\AsyncApi;
 /** @var AsyncApi $spec */
 $spec = require __DIR__ . '/includes/spec.php';
 
-$message = $spec->getOperations()['test_publish']->resolveMessages();
+$message = $spec->getOperations()['test_publish']->getMessages()[0];
 
-print_r($message);
+print_r($message->resolve());
